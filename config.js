1bc44f6a009c5f0a8c885b5cab769f7773f7c255
jafor918484-debug <jafor918484@gmail.com>
2026-09-04 00:21:27 +0600

    Add configuration for Telegram bot and MongoDB

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // আপনার টেলিগ্রাম বট টোকেন
+  BOT_TOKEN: process.env.BOT_TOKEN || "8856488614:AAHFdcDpX21sSy8vVicUy_H0cA3fW_LniBM",
+  
+  // Pella-র Environment Variable থেকে MONGO_URI নিবে
+  MONGO_URI: process.env.MONGO_URI || "mongodb+srv://mybot:sojib@@2@cluster0.ejv8z4g.mongodb.net/?appName=Cluster0",
+  
+  // প্রাথমিক সুপার এডমিন আইডি (টেলিগ্রাম চ্যাট আইডি)
+  SUPER_ADMINS: [6894518126] 
+};
